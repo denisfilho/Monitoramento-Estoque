2491b4db9f83968c9b5e33cebf2401bd83779eb1
Denis Ferreira <denisferreira93@hotmail.com>
2022-03-10 17:54:23 -0300

    Inserindo o DAO para o insumo

--- app/src/main/java/projeto/monitoramentoestoque/ListaInsumosActivity.java
+++ app/src/main/java/projeto/monitoramentoestoque/ListaInsumosActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class ListaInsumos extends AppCompatActivity {
+public class ListaInsumosActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
