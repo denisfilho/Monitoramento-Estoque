b3aa5116dc70e3e30bb7472af022a09a10b148a8
Denis Ferreira <denisferreira93@hotmail.com>
2022-03-21 11:48:35 -0300

    Criando a Activity para exibir as informações do insumo

---
+++ app/src/main/java/projeto/monitoramentoestoque/activity/InformacaoInsumoActivity.java
@@ -0,0 +1,16 @@
+package projeto.monitoramentoestoque.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import projeto.monitoramentoestoque.R;
+
+public class InformacaoInsumoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_informacao_insumo);
+    }
+}
