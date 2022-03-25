f3556b5608a5dde27792591cbf0c88f2e5df2919
Denis Ferreira <denisferreira93@hotmail.com>
2022-03-25 19:01:52 -0300

    Converter Calendar para String

--- app/src/main/java/projeto/monitoramentoestoque/activity/InformacaoInsumoActivity.java
+++ app/src/main/java/projeto/monitoramentoestoque/activity/InformacaoInsumoActivity.java
@@ -2,6 +2,7 @@ package projeto.monitoramentoestoque.activity;
 
 import static projeto.monitoramentoestoque.activity.InsumoActivityConstantes.CHAVE_INSUMO;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -12,6 +13,8 @@ import android.widget.TextView;
 import org.w3c.dom.Text;
 
 import java.io.Serializable;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import projeto.monitoramentoestoque.R;
 import projeto.monitoramentoestoque.model.Insumo;
@@ -37,7 +40,7 @@ public class InformacaoInsumoActivity extends AppCompatActivity {
             estoqueAtualInsumoRecebido.setText("Estoque Atual: " + Double.toString(insumoRecebido.getEstoqueAtual()));
 
             TextView ultimaAtualizacaoInsumoRecebido = findViewById(R.id.informacao_insumo_data_ultima_atualizacao);
-            ultimaAtualizacaoInsumoRecebido.setText("Última Atualização: ");
+            ultimaAtualizacaoInsumoRecebido.setText("Última Atualização: " + converterCalendarParaString(insumoRecebido));
 
             TextView botaoInserirNovaEntrada = findViewById(R.id.informacao_insumo_inserir_nova_entrada);
 
@@ -47,5 +50,10 @@ public class InformacaoInsumoActivity extends AppCompatActivity {
 
             TextView botaoHistoricoConsumo = findViewById(R.id.informacao_insumo_historico_consumo);
         }
+    }
+
+    @NonNull
+    private String converterCalendarParaString(Insumo insumoRecebido) {
+        return new SimpleDateFormat("dd/MM/yyyy").format(insumoRecebido.getDataUltimaAtualizacao().getTime());
     }
 }
