a6a3eba01d52e5f4cac6a3ab677f7d8e4b290f5d
Denis Ferreira <denisferreira93@hotmail.com>
2022-04-14 23:31:08 -0300

    Refatoração: eliminando alguns comandos desnecessários

--- app/src/main/java/projeto/monitoramentoestoque/activity/InsumoActivityConstantes.java
+++ app/src/main/java/projeto/monitoramentoestoque/activity/InsumoActivityConstantes.java
@@ -3,10 +3,11 @@ package projeto.monitoramentoestoque.activity;
 public interface InsumoActivityConstantes {
     String CHAVE_INSUMO = "insumo";
     String CHAVE_REQUISICAO = "requisicao";
+
     String CHAVE_REQUISICAO_INSERE_NOVA_ENTRADA = "Nova Entrada";
     String CHAVE_REQUISICAO_INSERE_NOVO_CONSUMO = "Novo Consumo";
+
     int CODIGO_REQUISICAO_INSERE_INSUMO = 1;
-    int CODIGO_REQUISICAO_EXIBE_INSUMO = 2;
 
     int CODIGO_RESULTADO_INSUMO_CRIADO = 2;
 }
--- app/src/main/java/projeto/monitoramentoestoque/dao/RoomInsumoDAO.java
+++ app/src/main/java/projeto/monitoramentoestoque/dao/RoomInsumoDAO.java
@@ -16,15 +16,6 @@ public interface RoomInsumoDAO {
     @Query("SELECT * FROM insumo")
     List<Insumo> todos();
 
-    @Query("SELECT * FROM insumo WHERE id IN (:insumoIds)")
-    List<Insumo> loadAllByIds(Long insumoIds);
-
-    @Query("SELECT id FROM insumo WHERE INSUMO_NOME is (:nomeInsumo) ")
-    Long encontraId(String nomeInsumo);
-
-    @Query("SELECT * FROM insumo WHERE id is (:idInsumo)")
-    Insumo encontraInsumo(Long idInsumo);
-
     @Insert
     void salvaInsumo(Insumo insumo);
 
